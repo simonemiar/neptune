c828fce1eee73778d8ad42b971bb1b6d14aa5e2c
Maja Sørensen <majabirchsorensen@gmail.com>
2021-09-14 14:53:41 +0200

    nyt burger X

--- burger.js
+++ burger.js
@@ -16,6 +16,6 @@ function toggleMenu() {
   if (erSkjult == true) {
     document.querySelector("#burgerBtn").textContent = "☰";
   } else {
-    document.querySelector("#burgerBtn").textContent = "✖";
+    document.querySelector("#burgerBtn").textContent = "⨉";
   }
 }
